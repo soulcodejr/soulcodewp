618a040c4ed35e5d3c5fb58a5e5656d24adb908a
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2016-10-03 10:40:09 -0300

    Added function show posts in index

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php get_header();?>
 <h1>Olá, Teste</h1>
+
+<!-- Content -->
+<?php
+if (have_posts()) {
+	while (have_posts()) {
+		the_post();
+		the_title();
+		the_content();
+	}
+}
+?>
+<!-- Content -->
+
 <?php get_footer();?>
