5d0d942e368d200063272e5748c25d3fd25576c3
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2016-10-04 12:01:58 -0300

    Remove $ of the function get_template

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <?php $home = $get_template_directory_uri(); ?>
+  <?php $home = get_template_directory_uri(); ?>
   <meta charset="utf-8">
   <?php wp_head();?>
 
