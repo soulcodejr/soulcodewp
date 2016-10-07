fca21a2f656fbc2ab7cb36e150d88ad253e53897
Rodolfo <rodolfo@uenf.br>
2016-10-07 20:36:04 -0300

    Added area of the video

--- pages/home.php
+++ pages/home.php
@@ -8,7 +8,7 @@
 <!-- OBJETIVO -->
 <div class="col-md-12 pt40 pb40">
 
-<div class="col-md-4 col-sm-6 feature-left-stack boxed">
+	<div class="col-md-4 col-sm-6 feature-left-stack boxed">
 		<div class="feature-box yellow">
 			<i class="icon-telescope size-4x color"></i> 
 			<div>
@@ -41,3 +41,43 @@
 </div>
 
 <!-- OBJETIVOS -->
+
+
+
+
+<!-- Video Embeds -->
+<section id="elements-videos yellow" >
+	<div class="container">  
+
+		<div class="col-md-12 text-center pb20">
+			<h1><strong>Video Embeds</strong></h1>
+			<p class="lead">Responsive iframe videos that will resize for any device.<br>Simply put your iframe inside video-container div.</p>
+		</div>
+
+		<div class="row">	
+
+			<!-- Youtube Half Column -->
+			<div class="col-md-4 mt20 mb20">
+			</br>
+			</div> 
+
+			<!-- Vimeo Half Column -->
+			<div class="col-md-4 mt20 mb20">
+				<div class="video-container">
+					<img src="img/video/thumbnail-vimeo.jpg" />
+					<iframe data-video-embed="http://player.vimeo.com/video/157100757"></iframe>
+				</div>
+			</div> 
+
+
+
+			<!-- Vimeo Half Column -->
+			<div class="col-md-4 mt20 mb20">
+			</br>
+			</div> 
+
+		</div>
+
+	</div>
+</section> 
+        <!-- End Video Embeds -->
