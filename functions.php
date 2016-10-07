7777c0e145cdc7b3cd42d1512cc6e52a21de9a70
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2016-10-06 23:58:49 -0300

    Addded the custom logo

--- functions.php
+++ functions.php
@@ -3,10 +3,6 @@
 add_theme_support('post-thumbnails');
 
 //VARIÁVEIS
-$args = array(
-  'public' => true
-);
-
 
 
 //functions
@@ -19,7 +15,7 @@ function client_register(){
   	 'name' => $namePlural,
   	 'name_singular' => $nameSingular,
   	 'add_new_item' => 'Adicionar novo ' . $nameSingular,
-  	 'edit_item' => 'Editar' . $nameSingular
+  	 'edit_item' => 'Editar ' . $nameSingular
    );
 
   $args = array(
@@ -33,6 +29,30 @@ function client_register(){
   register_post_type('clientes', $args);
 }
 
+function blog_register(){
+  $nameSingular = 'Blog';
+  $namePlural   = 'Blogs';
+  $description  = 'Blog da Soul Code';
+
+  $labels = array(
+    'name' => $namePlural,
+    'name_singular' => $nameSingular,
+    'add_new_item' => 'Adicionar novo ' . $nameSingular,
+    'edit_item' => 'Editar ' . $nameSingular
+
+    );
+
+  $args = array(
+    'labels' => $labels,
+    'public' => true,
+    'description' => $description,
+    'menu_icon' => 'dashicons-businessman'
+    );
+
+  register_post_type('blogs', $args);
+
+}
+
 function registration_menu_navation(){
   register_nav_menu('header-menu','main-menu');
 }
@@ -43,9 +63,25 @@ function gera_title(){
             the_title();
 }
 
+function theme_prefix_setup(){
+
+  $args = array(
+    'height' => 38,
+    'width'  => 170,
+    'flex-width' => true
+     );
+
+  add_theme_support('custom-logo', $args);
+}
+
+
+
+//END FUNCTIONS
 
 //Call function
 add_action('init', 'client_register' );
+add_action('init', 'blog_register');
 add_action('init', 'registration_menu_navation');
+add_action('after_setup_theme', 'theme_prefix_setup')
 
 ?>
--- header.php
+++ header.php
@@ -4,7 +4,7 @@
   <?php $home = get_template_directory_uri(); ?>
   <meta charset="utf-8">
   <title>
-  	<?php gera_title(); ?>
+    <?php gera_title(); ?>
 
   </title>
   <?php wp_head();?>
@@ -15,33 +15,45 @@
 <body>
 
  <!--Start Header-->
-        <section id="fixed-navbar">
-			<nav class="navbar navbar-default navbar-sticky" role="navigation">
-				<div class="container">
+ <section id="fixed-navbar">
+   <nav class="navbar navbar-default navbar-sticky" role="navigation">
+    <div class="container">
 
-					<div class="navbar-header">
-						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#main-nav">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-					    </button>
-                        <a class="navbar-brand" href="#"><img src="img/assets/logo.png" alt=""></a>
-					</div>
+     <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#main-nav">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#"><?php
 
-                    <div class="collapse navbar-collapse" id="main-nav">
-                        <ul class="nav navbar-nav  navbar-right rez-drop">
-                            <li>
-                            <?php
-                              $args = array(
-                                  'theme_location' => 'header-menu'
-                              	);
-                              wp_nav_menu($args);
-                            ?></li>
+       the_custom_logo();
 
-                      </ul>
-                    </div><!-- /.navbar-collapse -->
-				</div><!-- /.container -->
-			</nav>
-		</section>
+      if(!has_custom_logo()){
+        ?>
+
+        <h1><?php bloginfo('name'); ?></h1>
+
+        <?php
+      }
+
+        ?></a>
+      </div>
+
+      <div class="collapse navbar-collapse" id="main-nav">
+        <ul class="nav navbar-nav  navbar-right rez-drop">
+          <li>
+            <?php
+            $args = array(
+              'theme_location' => 'header-menu'
+              );
+            wp_nav_menu($args);
+            ?></li>
+
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container -->
+    </nav>
+  </section>
         <!--End Header-->
